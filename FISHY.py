33e5332024426c8acfe1c146684d098565584227
VIXISOY <vixis21@gmail.com>
2026-02-10 12:11:05 +0100

    Fixed FISHY.py

--- FISHY.py
+++ FISHY.py
@@ -3,4 +3,6 @@ from BASE import *
 if __name__ == '__main__':
     while True:
         FEMME.bank_deposit_item("gudgeon", 20)
+        FEMME.bank_deposit_item("algae", 20)
+        FEMME.bank_deposit_item("shell", 20)
         FEMME.farm_item("gudgeon", 20)
