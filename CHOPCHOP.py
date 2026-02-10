f7c0cda59e2946544959a9c17d2e7dc5a527806c
VIXISOY <vixis21@gmail.com>
2026-02-10 12:09:36 +0100

    Fixed CHOPCHOP.py

--- CHOPCHOP.py
+++ CHOPCHOP.py
@@ -3,4 +3,6 @@ from BASE import *
 if __name__ == '__main__':
     while True:
         BAGAR.bank_deposit_item("ash_wood", 20)
+        BAGAR.bank_deposit_item("sap", 20)
+        BAGAR.bank_deposit_item("apple", 20)
         BAGAR.farm_item("ash_wood", 20)
