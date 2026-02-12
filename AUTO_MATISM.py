3a07768b2cc65e5831f588e79de749fb84c0dda6
tomytomio <tom.gave27@gmail.com>
2026-02-12 09:19:31 +0100

    fix AUTO_MATISM

--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -1,11 +1,8 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-    while CHILD.get_character()["gearcrafting_level"] < 5:
-      CHILD.auto_craft("copper_helmet")
-    CHILD.auto_craft("feather_coat")
-    CHILD.auto_craft("copper_legs_armor")
     while CHILD.get_character()["weaponcrafting_level"] < 5:
       CHILD.auto_craft("copper_dagger")
+      CHILD.bank_deposit_item("copper_dagger",CHILD.get_item_quantity("copper_dagger"))
     CHILD.auto_craft("water_bow")
     
