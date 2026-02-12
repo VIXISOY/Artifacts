11fd43eb9ecc9769dab4a07901a2fe8e55bd0449
tomytomio <tom.gave27@gmail.com>
2026-02-12 12:33:36 +0100

    fix child

--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -3,9 +3,11 @@ from Source.BASE import *
 if __name__ == '__main__':
     while CHILD.get_character()["jewelrycrafting_level"] < 5:
       CHILD.auto_craft("copper_ring")
+      CHILD.bank_withdraw_item("copper_ring", get_bank_item_quantity("copper_ring"))
       CHILD.recycle("copper_ring",get_bank_item_quantity("copper_ring"))
     
     while True:
       CHILD.auto_craft("copper_legs_armor")
+      CHILD.bank_withdraw_item("copper_legs_armor", get_bank_item_quantity("copper_legs_armor"))
       CHILD.recycle("copper_legs_armor",get_bank_item_quantity("copper_legs_armor"))
     
