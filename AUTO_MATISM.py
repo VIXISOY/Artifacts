4e66fab408323460af5f207e63ae6ff2af2288ae
tomytomio <tom.gave27@gmail.com>
2026-02-12 12:29:11 +0100

    new child automatism

--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -1,8 +1,11 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-    while CHILD.get_character()["weaponcrafting_level"] < 5:
-      CHILD.auto_craft("copper_dagger")
-      CHILD.bank_deposit_item("copper_dagger",CHILD.get_item_quantity("copper_dagger"))
-    CHILD.auto_craft("water_bow")
+    while CHILD.get_character()["jewelrycrafting_level"] < 5:
+      CHILD.auto_craft("copper_ring")
+      CHILD.recycle("copper_ring",get_bank_item_quantity("copper_ring"))
+    
+    while True:
+      CHILD.auto_craft("copper_legs_armor")
+      CHILD.recycle("copper_legs_armor",get_bank_item_quantity("copper_legs_armor"))
     
