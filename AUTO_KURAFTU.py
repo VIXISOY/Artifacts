701780ae9fa54f1dbeacf48ce1815be474535ea4
tomytomio <tom.gave27@gmail.com>
2026-02-12 01:53:18 +0100

    auto pewpew

--- AUTO_KURAFTU.py
+++ AUTO_KURAFTU.py
@@ -1,14 +1,15 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
+    while True:
+        BAGAR.auto_craft("copper_ring")
+        
     BAGAR.auto_craft("copper_armor")
     BAGAR.bank_withdraw_item("copper_armor")
     BAGAR.equip("copper_armor")
 
     BAGAR.auto_craft("feather_coat")
 
-    while True:
-        BAGAR.auto_craft("copper_ring")
 
     BAGAR.auto_craft("life_amulet")
     BAGAR.bank_withdraw_item("life_amulet")
--- SUNFLOWER.py
+++
@@ -1,6 +0,0 @@
-from Source.BASE import *
-
-if __name__ == '__main__':
-    while True:
-        KRYST.bank_deposit_full_inventory()
-        KRYST.farm_item("sunflower", 20)
