f502672773e7ddb4afbfbe30e90b80e190677e09
VIXISOY <vixis21@gmail.com>
2026-02-13 17:11:46 +0100

    Update scripts

--- AUTO_KURAFTU.py
+++ AUTO_KURAFTU.py
@@ -12,3 +12,12 @@ if __name__ == '__main__':
         if count == 5:
             BAGAR.bank_deposit_full_inventory()
             count = 0
+
+    current = 10
+    while True:
+        CHILD.auto_craft_self_only("copper_legs_armor")
+        CHILD.recycle("copper_legs_armor", trigger)
+        current += 1
+        if current == 10:
+            CHILD.bank_deposit_full_inventory()
+            current = 0
