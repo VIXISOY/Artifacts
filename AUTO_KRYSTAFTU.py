80952cf3ff26a5c6568f567b5840f99051d27c65
tomytomio <tom.gave27@gmail.com>
2026-02-13 16:44:53 +0100

    new Krist

--- AUTO_KRYSTAFTU.py
+++ AUTO_KRYSTAFTU.py
@@ -4,10 +4,14 @@ if __name__ == '__main__':
     #KRYST.fighting_smart = True
     #for i in range(10):
     
+    while KRYST.get_character()["woodcutting_level"] < 10:
+        KRYST.farm_item("ash_wood",60)
+        KRYST.auto_craft("ash_plank",4)
+        KRYST.bank_deposit_full_inventory()
     while True:
-        if get_bank_item_quantity("algae") > 20:
-            KRYST.auto_craft("minor_health_potion",20)
-            KRYST.bank_deposit_full_inventory()
-        else:
-            KRYST.farm_item("nettle_leaf",80)
-            KRYST.bank_deposit_full_inventory()
+        KRYST.farm_item("spruce_wood",60)
+        KRYST.auto_craft("spruce_plank",6)
+        KRYST.bank_deposit_full_inventory()
+
+
+        
