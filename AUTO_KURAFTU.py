9e14108c712f1ed349a43cdaea67fb78126f540e
VIXISOY <vixis21@gmail.com>
2026-02-13 12:58:46 +0100

    deposit all sometimes to deposit byproducts

--- AUTO_KURAFTU.py
+++ AUTO_KURAFTU.py
@@ -2,6 +2,13 @@ from Source.BASE import *
 
 if __name__ == '__main__':
     BAGAR.bank_deposit_full_inventory()
+    count = 4
     while True:
         BAGAR.auto_craft_self_only("life_amulet")
         BAGAR.recycle("life_amulet")
+
+        count += 1
+
+        if count == 5:
+            BAGAR.bank_deposit_full_inventory()
+            count = 0
