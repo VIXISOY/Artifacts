888dfa6f76b24967a12b418cac0866f00b39e4a3
tomytomio <tom.gave27@gmail.com>
2026-02-13 15:21:49 +0100

    Revert "fix kryst"
    This reverts commit 80b88d44414f131320826e89bd547a1a7a1affee.

--- AUTO_KRYSTAFTU.py
+++ AUTO_KRYSTAFTU.py
@@ -5,8 +5,9 @@ if __name__ == '__main__':
     #for i in range(10):
     
     while True:
-        KRYST.bank_deposit_full_inventory()
         if get_bank_item_quantity("algae") > 20:
             KRYST.auto_craft("minor_health_potion",20)
+            KRYST.bank_deposit_full_inventory()
         else:
-            KRYST.farm_item("nettle_leaf",80)  
+            KRYST.farm_item("nettle_leaf",80)
+            KRYST.bank_deposit_full_inventory()
