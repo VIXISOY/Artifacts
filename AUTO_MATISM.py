fbfaf1f6f379bf3935a28d865c37d8d04d7a4418
tomytomio <tom.gave27@gmail.com>
2026-02-14 01:07:31 +0100

    automatism

--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -1,16 +1,15 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-    CHILD.fighting_smart = True
-    CHILD.bank_withdraw_item("spruce_plank",6)
-    CHILD.auto_craft_self_only("adventurer_helmet",2)
+  CHILD.fighting_smart = True
+  CHILD.bank_withdraw_item("spruce_plank",8)
+  CHILD.auto_craft_self_only("adventurer_vest",2)
+  CHILD.bank_deposit_full_inventory()
+  CHILD.auto_craft_self_only("iron_shield",2)
+  CHILD.bank_deposit_full_inventory()
+  CHILD.auto_craft_self_only("leather_legs_armor",2)
+  CHILD.bank_deposit_full_inventory()
+
+  while True:
     CHILD.bank_deposit_full_inventory()
-    CHILD.bank_withdraw_item("spruce_plank",8)
-    CHILD.auto_craft_self_only("adventurer_vest",2)
-    CHILD.bank_deposit_full_inventory()
-    CHILD.auto_craft_self_only("iron_shield",2)
-    CHILD.bank_deposit_full_inventory()
-    while CHILD.get_item_quantity("forest_ring") < 1:
-      CHILD.farm_item("forest_ring")
-    CHILD.equip("forest_ring")
-    CHILD.bank_deposit_full_inventory()
+    CHILD.auto_craft_self_only("iron_legs_armor")
