eb5130a671efa947a8fd04f5825b59941e2ee15a
tomytomio <tom.gave27@gmail.com>
2026-02-15 18:22:42 +0100

    WEAPON LVL 10 BABYYYY

--- AUTO_KURAFTU.py
+++ AUTO_KURAFTU.py
@@ -1,6 +1,6 @@
 from Source.BASE import *
 
-if __name__ == '__main__':
+def old():
     BAGAR.bank_deposit_full_inventory()
     count = 4
     while True:
@@ -21,3 +21,10 @@ if __name__ == '__main__':
         if current == 10:
             CHILD.bank_deposit_full_inventory()
             current = 0
+
+if __name__ == '__main__':
+        BAGAR.auto_craft("greater_wooden_staff",4)
+        BAGAR.auto_craft("iron_pickaxe",4)
+        BAGAR.auto_craft("iron_axe",4)
+        BAGAR.auto_craft("spruce_fishing_rod") 
+        BAGAR.auto_craft("leather_gloves") 
