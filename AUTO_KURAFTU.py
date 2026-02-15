8075a7eed58d309514a52ea1dab0088145fcf70e
tomytomio <tom.gave27@gmail.com>
2026-02-16 00:50:28 +0100

    AAA

--- AUTO_KURAFTU.py
+++ AUTO_KURAFTU.py
@@ -1,6 +1,7 @@
 from Source.BASE import *
 
-def old():
+if __name__ == '__main__':
+    
     BAGAR.bank_deposit_full_inventory()
     count = 4
     while True:
@@ -14,17 +15,3 @@ def old():
             count = 0
 
     current = 10
-    while True:
-        CHILD.auto_craft_self_only("copper_legs_armor")
-        CHILD.recycle("copper_legs_armor", trigger)
-        current += 1
-        if current == 10:
-            CHILD.bank_deposit_full_inventory()
-            current = 0
-
-if __name__ == '__main__':
-        BAGAR.auto_craft("greater_wooden_staff",4)
-        BAGAR.auto_craft("iron_pickaxe",4)
-        BAGAR.auto_craft("iron_axe",4)
-        BAGAR.auto_craft("spruce_fishing_rod") 
-        BAGAR.auto_craft("leather_gloves") 
--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -1,15 +1,6 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-  CHILD.fighting_smart = True
-  CHILD.bank_withdraw_item("spruce_plank",8)
-  CHILD.auto_craft_self_only("adventurer_vest",2)
-  CHILD.bank_deposit_full_inventory()
-  CHILD.auto_craft_self_only("iron_shield",2)
-  CHILD.bank_deposit_full_inventory()
-  CHILD.auto_craft_self_only("leather_legs_armor",2)
-  CHILD.bank_deposit_full_inventory()
-
   while True:
     CHILD.bank_deposit_full_inventory()
     CHILD.auto_craft_self_only("iron_legs_armor")
