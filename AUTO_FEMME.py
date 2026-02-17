68b5b555e9c7238c18d3d8817e0e27599d842887
VIXISOY <vixis21@gmail.com>
2026-02-17 16:53:41 +0100

    femme redondance

--- AUTO_FEMME.py
+++ AUTO_FEMME.py
@@ -6,5 +6,4 @@ if __name__ == '__main__':
     while True:
         FEMME.bank_deposit_full_inventory()
         FEMME.auto_craft("cooked_trout",50)
-        FEMME.bank_deposit_item("cooked_trout",50)
 
