2199dadcefe5640555594fd17b12536605e65a91
tomytomio <tom.gave27@gmail.com>
2026-02-18 22:12:43 +0100

    child

--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -1,9 +1,18 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-  CHILD.auto_craft("satchel",5)
-  CHILD.bank_deposit_full_inventory()
+  while CHILD.get_character()["jewelrycrafting_level"] < 10:
+    CHILD.auto_craft_self_only("life_amulet")
+    CHILD.recycle("life_amulet")
+    CHILD.bank_deposit_full_inventory(["feather","red_slimeball"])
+  CHILD.auto_craft_self_only("air_and_water_amulet")
+  CHILD.equip("air_and_water_amulet")
+  while CHILD.get_character()["jewelrycrafting_level"] < 15:
+    CHILD.auto_craft_self_only("iron_ring")
+    CHILD.recycle("life_amulet")
+    CHILD.bank_deposit_full_inventory(["iron_bar","wool"])
   while True:
-    CHILD.auto_craft_self_only("iron_boots")
-    CHILD.recycle("iron_boots")
-    CHILD.bank_deposit_full_inventory(["feather","iron_bar"])
+    CHILD.auto_craft_self_only("earth_ring")
+    CHILD.recycle("earth_ring")
+    CHILD.bank_deposit_full_inventory(["iron_bar","yellow_slimeball","flying_wing"])
+  
