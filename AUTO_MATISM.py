034a3522703cfc5b84e09be49a1d62a640bfa686
tomytomio <tom.gave27@gmail.com>
2026-02-19 22:21:06 +0100

    child recycle

--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
   CHILD.equip("air_and_water_amulet")
   while CHILD.get_character()["jewelrycrafting_level"] < 15:
     CHILD.auto_craft_self_only("iron_ring")
-    CHILD.recycle("life_amulet")
+    CHILD.recycle("iron_ring")
     CHILD.bank_deposit_full_inventory(["iron_bar","wool"])
   while True:
     CHILD.auto_craft_self_only("earth_ring")
     CHILD.recycle("earth_ring")
     CHILD.bank_deposit_full_inventory(["iron_bar","yellow_slimeball","flying_wing"])
-  
+   
