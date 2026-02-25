7422be247aff586ab08a45faf9f50aa7dbe77d9b
tomytomio <tom.gave27@gmail.com>
2026-02-25 22:16:13 +0100

    oo new server

--- AUTO_KRYSTAFTU.py
+++ AUTO_KRYSTAFTU.py
@@ -1,49 +1,6 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-  KRYST.auto_craft_self_only("fishing_net")
-  KRYST.bank_deposit_item("fishing_net")
-  KRYST.auto_craft_self_only("wooden_staff")
-  KRYST.auto_craft_self_only("apprentice_gloves")
-
-  copper_pickaxe = 0
-  copper_axe = 0
-  copper_dagger = 0
-  copper_boots = 0
-  copper_helmet = 0
-  wooden_shield = 0
-  while KRYST.get_character()["gearcrafting_level"] < 5:
-    if copper_pickaxe == 0 and  get_bank_item_quantity("copper_pickaxe") > 0:
-      KRYST.bank_withdraw_item("copper_pickaxe")
-      copper_pickaxe = 1
-
-    if copper_axe == 0 and  get_bank_item_quantity("copper_axe") > 0:
-      KRYST.bank_withdraw_item("copper_axe")
-      copper_axe = 1
-
-    if copper_dagger == 0 and  get_bank_item_quantity("copper_dagger") > 0:
-      KRYST.bank_withdraw_item("copper_dagger")
-      copper_dagger = 1
-      
-    if copper_boots == 0 and  get_bank_item_quantity("copper_boots") > 0:
-      KRYST.bank_withdraw_item("copper_boots")
-      KRYST.equip("copper_boots")
-      copper_boots = 1
-
-    if copper_helmet == 0 and  get_bank_item_quantity("copper_helmet") > 0:
-      KRYST.bank_withdraw_item("copper_helmet")
-      KRYST.equip("copper_helmet")
-      copper_helmet = 1
-
-    if wooden_shield == 0 and  get_bank_item_quantity("wooden_shield") > 0:
-      KRYST.bank_withdraw_item("wooden_shield")
-      KRYST.equip("wooden_shield")
-      wooden_shield = 1
-
-    KRYST.auto_craft_self_only("fishing_net")
-    KRYST.recycle("fishing_net")
-    KRYST.bank_deposit_full_inventory(["ash_plank"])
-  
   KRYST.auto_craft_self_only("water_bow",4)
   KRYST.bank_deposit_item("water_bow",3)
 
@@ -80,5 +37,3 @@ while True :
     CHILD.auto_craft_self_only("greater_wooden_staff")
     CHILD.recycle("greater_wooden_staff")
     CHILD.bank_deposit_full_inventory(["spruce_plank","blue_slimeball"])
-
-        
--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -1,38 +1,7 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-  CHILD.auto_craft_self_only("copper_boots",4)
-  CHILD.equip("copper_boots")
-  CHILD.bank_deposit_full_inventory()
-  CHILD.auto_craft_self_only("copper_helmet",4)
-  CHILD.equip("copper_helmet")
-  CHILD.bank_deposit_full_inventory()
-
-  copper_pickaxe = 0
-  copper_axe = 0
-  copper_dagger = 0
-  wooden_shield = 0
-  while CHILD.get_character()["gearcrafting_level"] < 5:
-    if copper_pickaxe == 0 and  get_bank_item_quantity("copper_pickaxe") > 0:
-      CHILD.bank_withdraw_item("copper_pickaxe")
-      copper_pickaxe = 1
-
-    if copper_axe == 0 and  get_bank_item_quantity("copper_axe") > 0:
-      CHILD.bank_withdraw_item("copper_axe")
-      copper_axe = 1
-
-    if copper_dagger == 0 and  get_bank_item_quantity("copper_dagger") > 0:
-      CHILD.bank_withdraw_item("copper_dagger")
-      copper_dagger = 1
-    
-    if wooden_shield == 0 and  get_bank_item_quantity("wooden_shield") > 0:
-      KRYST.bank_withdraw_item("wooden_shield")
-      KRYST.equip("wooden_shield")
-      wooden_shield = 1
-
-    CHILD.auto_craft_self_only("copper_helmet")
-    CHILD.recycle("copper_helmet")
-    CHILD.bank_deposit_full_inventory(["copper_bar"])
+  CHILD.bank_withdraw_item("wooden_shield")
   
   CHILD.auto_craft_self_only("copper_legs_armor",4)
   CHILD.equip("copper_legs_armor")
