030ca04cf20fec828dd8c1dda1c4c038bb6e9a4e
VIXISOY <vixis21@gmail.com>
2026-02-25 20:09:51 +0100

    test

--- GET.py
+++ GET.py
@@ -1,13 +1,7 @@
+print("JE DEMARRE")
+
 import time
 
-print("AVANT IMPORT")
-
-from Source.BASE import *
-
-print("APRES IMPORT")
-
-if __name__ == '__main__':
-    print("MAIN START")
-    while True:
-        print("TEST LOG")
-        time.sleep(1)
+while True:
+    print("TEST LOG")
+    time.sleep(1)
