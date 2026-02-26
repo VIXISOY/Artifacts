931e7392a5dde15250565855f191dfdbbffa8e9c
VIXISOY <vixis21@gmail.com>
2026-02-26 14:53:24 +0100

    scripts

--- AUTO_KURAFTU.py
+++ AUTO_KURAFTU.py
@@ -1,37 +1,16 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-    print(BAGAR.get_character()["weapon_slot"])
+
+    BAGAR.auto_craft("sticky_sword",equip=True)
+    BAGAR.recycle("copper_dagger")
 
 
-    BAGAR.bank_deposit_full_inventory()
-
-    while BAGAR.get_character()["weaponcrafting_level"] < 5:
-        BAGAR.auto_craft("copper_dagger", recycle=True)
 
     BAGAR.bank_deposit_full_inventory()
-    BAGAR.auto_craft("sticky_sword", equip=True)
-    BAGAR.auto_craft("sticky_sword")
-    BAGAR.auto_craft("sticky_sword")
-    BAGAR.auto_craft("sticky_sword")
-    BAGAR.bank_deposit_full_inventory()
 
-    copper_armor = 0
-    copper_legs_armor = 0
-    water_bow = 0
-    while BAGAR.get_character()["weaponcrafting_level"] < 10:
+    while BAGAR.get_character()["weaponcrafting_level"] < 15:
         BAGAR.auto_craft("sticky_sword", recycle=True)
-        if copper_armor == 0 and get_bank_item_quantity("copper_armor") > 0:
-            BAGAR.bank_withdraw_item("copper_armor")
-            BAGAR.equip("copper_armor")
-            copper_armor = 1
-        if copper_legs_armor == 0 and get_bank_item_quantity("copper_legs_armor") > 0:
-            BAGAR.bank_withdraw_item("copper_legs_armor")
-            BAGAR.equip("copper_legs_armor")
-            copper_legs_armor = 1
-        if water_bow == 0 and get_bank_item_quantity("water_bow") > 0:
-            BAGAR.bank_withdraw_item("water_bow")
-            water_bow = 1
 
 
 
--- CHOPCHOP.py
+++ CHOPCHOP.py
@@ -2,27 +2,14 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
-    while CHOPA.get_character()["gearcrafting_level"] < 5:
-        CHOPA.auto_craft("wooden_shield", recycle=True)
+    CHOPA.bank_deposit_full_inventory()
+    CHOPA.recycle("copper_dagger")
 
-    copper_armor = 0
-    copper_legs_armor = 0
     water_bow = 0
-    sticky_sword = 0
     while CHOPA.get_character()["gearcrafting_level"] < 10:
-        if copper_armor == 0 and get_bank_item_quantity("copper_armor") > 0:
-            CHOPA.bank_withdraw_item("copper_armor")
-            CHOPA.equip("copper_armor")
-            copper_armor = 1
-        if copper_legs_armor == 0 and get_bank_item_quantity("copper_legs_armor") > 0:
-            CHOPA.bank_withdraw_item("copper_legs_armor")
-            CHOPA.equip("copper_legs_armor")
-            copper_legs_armor = 1
         if water_bow == 0 and get_bank_item_quantity("water_bow") > 0:
             CHOPA.bank_withdraw_item("water_bow")
             water_bow = 1
-        if sticky_sword == 0 and get_bank_item_quantity("sticky_sword") > 0:
-            CHOPA.bank_withdraw_item("sticky_sword")
-            sticky_sword = 1
         CHOPA.auto_craft("feather_coat", recycle=True)
+        CHOPA.bank_deposit_full_inventory()
 
