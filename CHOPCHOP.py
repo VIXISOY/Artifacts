a24516be07a140907418288d2df62eca17c7b21d
VIXISOY <vixis21@gmail.com>
2026-02-27 17:22:41 +0100

    script

--- CHOPCHOP.py
+++ CHOPCHOP.py
@@ -2,8 +2,8 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
-    while CHOPA.get_character()["gearcrafting_level"] < 20:
-        CHOPA.farm_item("red_slimeball")
+    #while CHOPA.get_character()["gearcrafting_level"] < 20:
+    #    CHOPA.farm_item("red_slimeball")
 
     CHOPA.auto_craft("adventurer_vest",equip=True)
     CHOPA.auto_craft("adventurer_vest")
