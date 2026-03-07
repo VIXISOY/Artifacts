a484c261199d3d0db67ee57a10da8eb38cd177c9
tomytomio <tom.gave27@gmail.com>
2026-03-07 02:03:56 +0100

    no more shield (hero ?)

--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -2,16 +2,6 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
-  CHILD.auto_craft_self_only("iron_shield")
-  CHILD.bank_deposit_item("iron_shield")
-  CHILD.auto_craft_self_only("iron_shield")
-  CHILD.bank_deposit_item("iron_shield")
-  CHILD.auto_craft_self_only("iron_shield")
-  CHILD.bank_deposit_item("iron_shield")
-  CHILD.auto_craft_self_only("iron_shield")
-  CHILD.equip("iron_shield")
-  CHILD.bank_deposit_full_inventory()
-
   while True :
     CHILD.auto_craft_self_only("lucky_wizard_hat")
     CHILD.recycle("lucky_wizard_hat")
