debb18b337dffe18a78fda2e374abc4197988b6a
VIXISOY <vixis21@gmail.com>
2026-03-08 16:37:17 +0100

    Fix auto tasks fr fr fr fr

--- CHOPCHOP.py
+++ CHOPCHOP.py
@@ -4,6 +4,11 @@ if __name__ == '__main__':
 
     #while CHOPA.get_character()["gearcrafting_level"] < 20:
     #    CHOPA.farm_item("red_slimeball")
+    while get_bank_item_quantity("adventurer_boots") < 4:
+        CHOPA.auto_craft("adventurer_boots")
+
+    while get_bank_item_quantity("adventurer_pants") < 4:
+        CHOPA.auto_craft("adventurer_pants")
 
 
     while CHOPA.get_character()["gearcrafting_level"] < 25:
