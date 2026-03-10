52aee43bf2f372d709e67caa1dddbc7583373087
Tom <tom@tom.debian>
2026-03-10 10:42:29 +0100

    le peuple veux et le peuple aura

--- AUTO_KRYSTAFTU.py
+++ AUTO_KRYSTAFTU.py
@@ -1,7 +1,17 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
+  while get_bank_item_quantity("mushstaff") < 4:
+    KRYST.auto_craft_self_only("mushstaff")
+    KRYST.bank_deposit_item("mushstaff")
+    KRYST.bank_deposit_full_inventory(["spruce_plank,mushroom,green_cloth,jasper_crystal,tasks_coin"])
+
+  while get_bank_item_quantity("mushmush_bow") < 4:
+    KRYST.auto_craft_self_only("mushmush_bow")
+    KRYST.bank_deposit_item("mushmush_bow")
+    KRYST.bank_deposit_full_inventory(["spruce_plank,wolf_hair,mushroom,jasper_crystal,tasks_coin"])
+
   while True :
-    KRYST.auto_craft_self_only("greater_wooden_staff")
-    KRYST.recycle("greater_wooden_staff")
-    KRYST.bank_deposit_full_inventory(["spruce_plank","blue_slimeball"])
+    KRYST.auto_craft_self_only("mushmush_bow")
+    KRYST.recycle("mushmush_bow")
+    KRYST.bank_deposit_full_inventory(["spruce_plank,wolf_hair,mushroom,jasper_crystal,tasks_coin"])
