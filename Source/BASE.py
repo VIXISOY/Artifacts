595e097bcc471776cd0053ca10504d8d039e7aec
VIXISOY <vixis21@gmail.com>
2026-03-10 11:09:03 +0100

    scripts

--- Source/BASE.py
+++ Source/BASE.py
@@ -395,7 +395,7 @@ class Character:
             self.task_accept("monster")
 
         char = self.get_character()
-        if get_monster(char["task"])["level"] >= char["level"]-2 :
+        if get_monster(char["task"])["data"]["level"] >= char["level"]-2 :
             self.task_cancel()
 
         print(f"===TASK FARM=== {char["task_progress"]}/{char["task_total"]} {char["task"]}")
