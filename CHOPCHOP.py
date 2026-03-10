a27ba86cad8da3ee70d3ccc05d2bf0e516036e9d
VIXISOY <vixis21@gmail.com>
2026-03-10 10:33:43 +0100

    scripts

--- CHOPCHOP.py
+++ CHOPCHOP.py
@@ -4,12 +4,6 @@ if __name__ == '__main__':
 
     #while CHOPA.get_character()["gearcrafting_level"] < 20:
     #    CHOPA.farm_item("red_slimeball")
-    while get_bank_item_quantity("adventurer_boots") < 4:
-        CHOPA.auto_craft("adventurer_boots")
-
-    while get_bank_item_quantity("adventurer_pants") < 4:
-        CHOPA.auto_craft("adventurer_pants")
-
 
     while CHOPA.get_character()["gearcrafting_level"] < 25:
         CHOPA.auto_craft("adventurer_boots",recycle=True)
