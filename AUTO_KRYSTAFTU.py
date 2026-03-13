4457abc2d449ba5c8f87f4dedc82808d796121db
tomytomio <tom.gave27@gmail.com>
2026-03-13 11:35:13 +0100

    ..

--- AUTO_KRYSTAFTU.py
+++ AUTO_KRYSTAFTU.py
@@ -3,7 +3,7 @@ from Source.BASE import *
 if __name__ == '__main__':
   while get_bank_item_quantity("jasper_crystal") < 7:
     KRYST.farm_item("jasper_crystal")
-    KRYST.bank_deposit_item("jasper_crystal")
+    KRYST.bank_deposit_item("jasper_crystal",KRYST.get_item_quantity("jasper_crystal"))
 
   while True :
     KRYST.auto_craft_self_only("forest_whip")
