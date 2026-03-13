0e84f82864b7d519f104e069c91ea568c043f420
tomytomio <tom.gave27@gmail.com>
2026-03-13 11:54:00 +0100

    ....

--- AUTO_KRYSTAFTU.py
+++ AUTO_KRYSTAFTU.py
@@ -1,7 +1,7 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-  while get_bank_item_quantity("jasper_crystal") < 7:
+  while get_bank_item_quantity("jasper_crystal") < 12:
     KRYST.farm_item("jasper_crystal")
     KRYST.bank_deposit_item("jasper_crystal",KRYST.get_item_quantity("jasper_crystal"))
 
--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -1,13 +1,23 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-  while get_bank_item_quantity("mushmush_wizard_hat") < 3 :
-    CHILD.auto_craft_self_only("mushmush_wizard_hat")
-    CHILD.bank_deposit_item("mushmush_wizard_hat")
+  while get_bank_item_quantity("lucky_wizard_hat") < 4 :
+    CHILD.auto_craft_self_only("lucky_wizard_hat")
+    CHILD.bank_deposit_item("lucky_wizard_hat")
+    
+  while get_bank_item_quantity("steel_helm") < 4 :
+    CHILD.auto_craft_self_only("steel_helm")
+    CHILD.bank_deposit_item("steel_helm")
 
-  while get_bank_item_quantity("mushmush_jacket") < 3 :
-    CHILD.auto_craft_self_only("mushmush_jacket")
-    CHILD.bank_deposit_item("mushmush_jacket")
+  while get_bank_item_quantity("steel_armor") < 4 :
+    CHILD.auto_craft_self_only("steel_armor")
+    CHILD.bank_deposit_item("steel_armor")
+
+  while get_bank_item_quantity("leg_armor") < 4 :
+    CHILD.auto_craft_self_only("leg_armor")
+    CHILD.bank_deposit_item("leg_armor")
+  
+
 
   while True :
     CHILD.auto_craft_self_only("lucky_wizard_hat")
