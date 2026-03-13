796e6184033cb7831441e3443c867ea15a66e68f
tomytomio <tom.gave27@gmail.com>
2026-03-13 11:38:39 +0100

    ...

--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -4,14 +4,10 @@ if __name__ == '__main__':
   while get_bank_item_quantity("mushmush_wizard_hat") < 3 :
     CHILD.auto_craft_self_only("mushmush_wizard_hat")
     CHILD.bank_deposit_item("mushmush_wizard_hat")
-  CHILD.auto_craft_self_only("mushmush_wizard_hat")
-  CHILD.equip("mushmush_wizard_hat")
 
   while get_bank_item_quantity("mushmush_jacket") < 3 :
     CHILD.auto_craft_self_only("mushmush_jacket")
     CHILD.bank_deposit_item("mushmush_jacket")
-  CHILD.auto_craft_self_only("mushmush_jacket")
-  CHILD.equip("mushmush_jacket")
 
   while True :
     CHILD.auto_craft_self_only("lucky_wizard_hat")
