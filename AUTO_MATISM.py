346a049a93e335f83623132d26b789dcaac1d14d
tomytomio <tom.gave27@gmail.com>
2026-03-13 13:49:49 +0100

    .....

--- AUTO_MATISM.py
+++ AUTO_MATISM.py
@@ -1,10 +1,6 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-  while get_bank_item_quantity("lucky_wizard_hat") < 4 :
-    CHILD.auto_craft_self_only("lucky_wizard_hat")
-    CHILD.bank_deposit_item("lucky_wizard_hat")
-    
   while get_bank_item_quantity("steel_helm") < 4 :
     CHILD.auto_craft_self_only("steel_helm")
     CHILD.bank_deposit_item("steel_helm")
@@ -17,7 +13,8 @@ if __name__ == '__main__':
     CHILD.auto_craft_self_only("leg_armor")
     CHILD.bank_deposit_item("leg_armor")
   
-
+  while get_bank_item_quantity("lucky_wizard_hat") < 4 :
+    CHILD.auto_craft("lucky_wizard_hat")
 
   while True :
     CHILD.auto_craft_self_only("lucky_wizard_hat")
