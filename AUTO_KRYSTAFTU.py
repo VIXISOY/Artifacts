10db4d85704abadf389c2d122efa77014c65d73e
tomytomio <tom.gave27@gmail.com>
2026-03-13 11:34:16 +0100

    .

--- AUTO_KRYSTAFTU.py
+++ AUTO_KRYSTAFTU.py
@@ -3,8 +3,7 @@ from Source.BASE import *
 if __name__ == '__main__':
   while get_bank_item_quantity("jasper_crystal") < 7:
     KRYST.farm_item("jasper_crystal")
-    KRYST.bank_deposit_item("mushstaff")
-    KRYST.bank_deposit_full_inventory(["spruce_plank","mushroom","green_cloth","jasper_crystal","tasks_coin"])
+    KRYST.bank_deposit_item("jasper_crystal")
 
   while True :
     KRYST.auto_craft_self_only("forest_whip")
