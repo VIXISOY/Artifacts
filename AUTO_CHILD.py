29916b33c1e7fb06ee741ff5c7e0803b80789cbe
tomytomio <tom.gave27@gmail.com>
2026-03-14 17:09:09 +0100

    child

--- AUTO_CHILD.py
+++ AUTO_CHILD.py
@@ -1,19 +1,13 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-  while get_bank_item_quantity("steel_helm") < 4 :
-    CHILD.auto_craft_self_only("steel_helm")
-    CHILD.bank_deposit_item("steel_helm")
 
-  while get_bank_item_quantity("steel_armor") < 4 :
-    CHILD.auto_craft_self_only("steel_armor")
-    CHILD.bank_deposit_item("steel_armor")
 
   while get_bank_item_quantity("hard_leather_pants") < 4 :
     CHILD.auto_craft_self_only("hard_leather_pants")
     CHILD.bank_deposit_item("hard_leather_pants")
 
   while True :
-    CHILD.auto_craft_self_only("steel_helm")
-    CHILD.recycle("steel_helm")
-    CHILD.bank_deposit_full_inventory(["steel_bar","ogre_skin","wolf_bone","cloth"])
+    CHILD.auto_craft_self_only("hard_leather_pants")
+    CHILD.recycle("hard_leather_pants")
+    CHILD.bank_deposit_full_inventory(["steel_bar","green_cloth","hard_leather","skeleton_skull"])
