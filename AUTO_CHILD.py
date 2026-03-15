7e9da99e25ee92359146405aa8ba31e54facc03f
tomytomio <tom.gave27@gmail.com>
2026-03-15 17:21:11 +0100

    A

--- AUTO_CHILD.py
+++ AUTO_CHILD.py
@@ -3,5 +3,5 @@ from Source.BASE import *
 if __name__ == '__main__':
 
   while get_bank_item_quantity("jasper_crystal") < 12:
-    KRYST.farm_item("jasper_crystal")
-    KRYST.bank_deposit_item("jasper_crystal",KRYST.get_item_quantity("jasper_crystal"))
+    CHILD.farm_item("jasper_crystal")
+    CHILD.bank_deposit_item("jasper_crystal",CHILD.get_item_quantity("jasper_crystal"))
