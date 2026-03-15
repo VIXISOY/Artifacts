5e92f4fea8f492ed7b34606a9b25c764957956ea
tomytomio <tom.gave27@gmail.com>
2026-03-15 18:42:07 +0100

    kryst slime shield

--- AUTO_KRYST.py
+++ AUTO_KRYST.py
@@ -1,9 +1,13 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
+  KRYST.auto_craft("slime_shield")
+  KRYST.bank_deposit_item("slime_shield",1)
+  KRYST.bank_deposit_full_inventory()
+
   while True:
-    KRYST.farm_item("tasks_coin")
-    KRYST.bank_deposit_item("tasks_coin",KRYST.get_item_quantity("tasks_coin"))
+    KRYST.farm_item("pig_skin")
+    KRYST.bank_deposit_item("pig_skin",KRYST.get_item_quantity("pig_skin"))
 
   while True :
     KRYST.auto_craft_self_only("forest_whip")
