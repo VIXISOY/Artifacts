9d4bddf164c9f86b6afb279462f119412047d7dc
tomytomio <tom.gave27@gmail.com>
2026-03-15 17:32:18 +0100

    child/kryst au charbon

--- AUTO_CHILD.py
+++ AUTO_CHILD.py
@@ -2,6 +2,6 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
-  while get_bank_item_quantity("jasper_crystal") < 12:
-    CHILD.farm_item("jasper_crystal")
-    CHILD.bank_deposit_item("jasper_crystal",CHILD.get_item_quantity("jasper_crystal"))
+  while True:
+    CHILD.farm_item("tasks_coin")
+    CHILD.bank_deposit_item("tasks_coin",CHILD.get_item_quantity("tasks_coin"))
--- AUTO_KRYST.py
+++ AUTO_KRYST.py
@@ -1,9 +1,9 @@
 from Source.BASE import *
 
 if __name__ == '__main__':
-  while get_bank_item_quantity("jasper_crystal") < 12:
-    KRYST.farm_item("jasper_crystal")
-    KRYST.bank_deposit_item("jasper_crystal",KRYST.get_item_quantity("jasper_crystal"))
+  while True:
+    KRYST.farm_item("tasks_coin")
+    KRYST.bank_deposit_item("tasks_coin",KRYST.get_item_quantity("tasks_coin"))
 
   while True :
     KRYST.auto_craft_self_only("forest_whip")
