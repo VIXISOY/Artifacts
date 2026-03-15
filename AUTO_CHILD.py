6aa2dfcea48d09e687b56ad619aedc9603bad5a5
tomytomio <tom.gave27@gmail.com>
2026-03-15 16:08:24 +0100

    child need to quest

--- AUTO_CHILD.py
+++ AUTO_CHILD.py
@@ -2,12 +2,6 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
-
-  while get_bank_item_quantity("hard_leather_pants") < 4 :
-    CHILD.auto_craft_self_only("hard_leather_pants")
-    CHILD.bank_deposit_item("hard_leather_pants")
-
-  while True :
-    CHILD.auto_craft_self_only("hard_leather_pants")
-    CHILD.recycle("hard_leather_pants")
-    CHILD.bank_deposit_full_inventory(["steel_bar","green_cloth","hard_leather","skeleton_skull"])
+  while get_bank_item_quantity("jasper_crystal") < 12:
+    KRYST.farm_item("jasper_crystal")
+    KRYST.bank_deposit_item("jasper_crystal",KRYST.get_item_quantity("jasper_crystal"))
