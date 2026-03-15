08d2558abdc9d08547739672c4bbcb0260faf223
VIXISOY <vixis21@gmail.com>
2026-03-15 02:48:55 +0100

    scripts

--- AUTO_BAGAR.py
+++ AUTO_BAGAR.py
@@ -2,17 +2,10 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
-    BAGAR.auto_craft("iron_axe")
-
-    while get_bank_item_quantity("skull_staff") < 4:
-        BAGAR.auto_craft("skull_staff")
-
-    while get_bank_item_quantity("battlestaff") < 4:
-        BAGAR.auto_craft("battlestaff")
     # BAGAR.auto_craft("iron_pickaxe")
 
 
-    # while True:
-    #     BAGAR.auto_craft("iron_pickaxe",recycle=True)
+    while True:
+         BAGAR.auto_craft("battlestaff",recycle=True)
 
 
