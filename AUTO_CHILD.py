1c86733b62885731c67175f3e9c4ac46872e1304
VIXISOY <vixis21@gmail.com>
2026-03-17 14:03:37 +0100

    script

--- AUTO_CHILD.py
+++ AUTO_CHILD.py
@@ -3,13 +3,13 @@ from Source.BASE import *
 if __name__ == '__main__':
 
   while get_bank_item_quantity("skeleton_armor") < 4:
-    CHILD.auto_craft_self_only("skeleton_armor")
+    CHILD.auto_craft("skeleton_armor")
 
   while get_bank_item_quantity("skeleton_pants") < 4:
-    CHILD.auto_craft_self_only("skeleton_pants")
+    CHILD.auto_craft("skeleton_pants")
 
   while get_bank_item_quantity("tromatising_mask-pants") < 4:
-    CHILD.auto_craft_self_only("tromatising_mask")
+    CHILD.auto_craft("tromatising_mask")
 
   while True:
     CHILD.auto_craft_self_only("hard_leather_pants")
