6fd6f2c00bbd624e187583398d1f1b290fce5a3b
VIXISOY <vixis21@gmail.com>
2026-03-17 11:11:08 +0100

    script

--- AUTO_BAGAR.py
+++ AUTO_BAGAR.py
@@ -2,11 +2,12 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
-    while get_bank_item_quantity("fire_and_earth_amulet") < 4:
-        BAGAR.auto_craft("fire_and_earth_amulet")
+    # while get_bank_item_quantity("fire_and_earth_amulet") < 4:
+    #     BAGAR.auto_craft("fire_and_earth_amulet")
 
 
     while True:
-         BAGAR.auto_craft("battlestaff",recycle=True)
+         BAGAR.farm_item("tasks_coin")
+         BAGAR.bank_deposit_item("tasks_coin", BAGAR.get_item_quantity("tasks_coin"))
 
 
