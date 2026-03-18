597962c5f6cb2217077514b07c7444a689676c2b
tomytomio <tom.gave27@gmail.com>
2026-03-18 18:41:03 +0100

    child no thrauma

--- AUTO_CHILD.py
+++ AUTO_CHILD.py
@@ -2,16 +2,5 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
-  while get_bank_item_quantity("skeleton_armor") < 4:
-    CHILD.auto_craft("skeleton_armor")
-
-  while get_bank_item_quantity("skeleton_pants") < 4:
-    CHILD.auto_craft("skeleton_pants")
-
-  while get_bank_item_quantity("tromatising_mask") < 4:
-    CHILD.auto_craft("tromatising_mask")
-
   while True:
-    CHILD.auto_craft_self_only("hard_leather_pants")
-    CHILD.recycle("hard_leather_pants")
-    CHILD.bank_deposit_full_inventory(["steel_bar","green_cloth","hard_leather","skeleton_skull"])
+    CHILD.auto_craft("hard_leather_pants", recycle=True)
