e5181dbc773ba22e697ef687ba7639fb6e2eb31f
VIXISOY <vixis21@gmail.com>
2026-03-21 17:27:28 +0100

    script

--- AUTO_CHOPA.py
+++ AUTO_CHOPA.py
@@ -2,5 +2,5 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
-    while CHOPA.get_character()["gearcrafting_level"] < 25:
+    while CHOPA.get_character()["gearcrafting_level"] < 35:
         CHOPA.auto_craft("hard_leather_boots",recycle=True)
