7a9286655b94fcbb3dc30cbe9824c7462b6502c7
VIXISOY <vixis21@gmail.com>
2026-03-23 09:44:09 +0100

    script

--- AUTO_FEMME.py
+++ AUTO_FEMME.py
@@ -5,6 +5,8 @@ if __name__ == '__main__':
     #KRYST.get_character()["fishing_level"] < 20
 
     while True :
-        FEMME.bank_deposit_full_inventory(consumable=True)
-        FEMME.auto_craft("cooked_trout",50)
-        FEMME.auto_craft("small_health_potion", 30)
+        FEMME.auto_craft("earth_boost_potion", 30)
+        # FEMME.bank_deposit_full_inventory(consumable=True)
+        # FEMME.auto_craft("cooked_trout",50)
+
+        #FEMME.auto_craft("small_health_potion", 30)
