afebfe5d4e80b019533ebf42112cf5d187a1663c
VIXISOY <vixis21@gmail.com>
2026-03-24 09:34:12 +0100

    script

--- AUTO_CHILD.py
+++ AUTO_CHILD.py
@@ -2,5 +2,8 @@ from Source.BASE import *
 
 if __name__ == '__main__':
 
+  while get_bank_item_quantity("piggy_helmet") < 3:
+    CHILD.auto_craft("piggy_helmet")
+
   while True:
     CHILD.auto_craft("piggy_helmet", recycle=True)
