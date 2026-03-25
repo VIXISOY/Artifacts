2ca6acf4d9ad9b33345fdc5652a13dc69d728fb3
VIXISOY <vixis21@gmail.com>
2026-03-25 16:39:19 +0100

    script

--- AUTO_CHILD.py
+++ AUTO_CHILD.py
@@ -3,8 +3,5 @@ CHILD = Character("CHILD")
 
 if __name__ == '__main__':
 
-  while get_bank_item_quantity("piggy_helmet") < 3:
-    CHILD.auto_craft("piggy_helmet")
-
   while True:
     CHILD.auto_craft("piggy_helmet", recycle=True)
