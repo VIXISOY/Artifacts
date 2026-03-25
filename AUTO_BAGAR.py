f45e5946894a303201348d18c12c2fd87abb1226
VIXISOY <vixis21@gmail.com>
2026-03-25 16:34:31 +0100

    script

--- AUTO_BAGAR.py
+++ AUTO_BAGAR.py
@@ -4,18 +4,6 @@ BAGAR = Character("BAGAR")
 
 if __name__ == '__main__':
 
-    while BAGAR.get_character()["jewelrycrafting_level"] < 20:
-        BAGAR.auto_craft("life_ring",recycle=True)
-
-    while get_bank_item_quantity("ring_of_chance") < 3:
-        BAGAR.auto_craft("ring_of_chance")
-
-    while get_bank_item_quantity("skull_wand") < 4:
-        BAGAR.auto_craft("skull_wand")
-
-    while get_bank_item_quantity("skull_amulet") < 4:
-        BAGAR.auto_craft("skull_amulet")
-
     while True:
         BAGAR.farm_item("tasks_coin")
         BAGAR.bank_deposit_item("tasks_coin", KRYST.get_item_quantity("tasks_coin"))
