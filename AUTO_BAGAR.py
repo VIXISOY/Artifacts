490d6c41ec3f10cf230e900687b191a568a8d4af
VIXISOY <vixis21@gmail.com>
2026-03-26 09:03:38 +0100

    script

--- AUTO_BAGAR.py
+++ AUTO_BAGAR.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     while True:
         BAGAR.farm_item("tasks_coin")
-        BAGAR.bank_deposit_item("tasks_coin", KRYST.get_item_quantity("tasks_coin"))
+        BAGAR.bank_deposit_item("tasks_coin", BAGAR.get_item_quantity("tasks_coin"))
         # BAGAR.boss_farm("king_slime", CHOPA,quantity=20)
         # BAGAR.farm_item("tasks_coin")
         # BAGAR.bank_deposit_item("tasks_coin", BAGAR.get_item_quantity("tasks_coin"))
